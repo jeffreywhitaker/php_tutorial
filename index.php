ed0492d759c4e3ea414d4f2efc2b41581bb21e10
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-11-11 09:58:35 -0800

    display data from server

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (!$conn) {
 }
 
 // write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make query and get result
 $result = mysqli_query($conn, $sql);
@@ -22,7 +22,7 @@ mysqli_free_result($result);
 // close connection
 mysqli_close($conn);
 
-print_r($pizzas);
+// print_r($pizzas);
 
 ?>
 
@@ -31,6 +31,28 @@ print_r($pizzas);
 <html lang="en">
 
 <?php include 'templates/header.php'; ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+<div class="row">
+  
+  <?php foreach ($pizzas as $pizza) { ?>
+    <div class="col s6 md3">
+      <div class="card z-depth-0">
+        <div class="card-content center">
+          <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+          <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+        </div>
+        <div class="card-action right-align">
+          <a href="#" class="brand-text">more info</a>
+        </div>
+      </div>
+    </div>
+
+  <?php } ?>
+</div>
+
+</div>
 <?php include 'templates/footer.php'; ?>
 
 </body>
